100b131cb513ff5d7fe2dd53359a3c8c6a0d1c10
DallasCarraher <dallas.carraher@gmail.com>
2024-12-01 11:48:12 -0800

    part 1

--- 1/main_test.ts
+++ 1/main_test.ts
@@ -1,6 +1,6 @@
 import { assertEquals } from "@std/assert";
-import { add } from "./main.ts";
 
+// Write a test that
 Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
+  assertEquals(5, 5);
 });
